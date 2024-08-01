d03408e7fd9f8f7c5a04a499f2f9cc4a86c2bde6
techleonic <techleonic@gmail.com>
2024-08-01 15:02:07 -0600

    file rename Pathlib

---
+++ Pathlib_files/Rename_file.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+root_dir = Path("files")
+file_paths = root_dir.iterdir()
+
+for path in file_paths:
+    new_filename = "new-" + path.stem + path.suffix
+    new_filepath = path.with_name(new_filename)
+    print(new_filepath)
+    path.rename(new_filepath)
