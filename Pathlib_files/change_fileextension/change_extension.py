66268872a837e44ad99e3cdf2772ffdb86c98456
techleonic <techleonic@gmail.com>
2024-08-13 12:35:11 -0600

    change files extensions

---
+++ Pathlib_files/change_fileextension/change_extension.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+#create the files
+# for num in range(10,21):
+#     with open(f"files/{num}.txt", "w") as file:
+#         file.write("")
+
+root_path = Path("files")
+
+for path in root_path.glob("*.txt"):
+    if path.is_file():
+        new_filepath = path.with_suffix(".csv")
+        path.rename(new_filepath)
