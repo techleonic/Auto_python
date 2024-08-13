2ff7e70f231e4f92bb4bcf983b21a98dc1f17b6f
techleonic <techleonic@gmail.com>
2024-08-13 10:23:39 -0600

    add datetime into file

--- Pathlib_files/date_all_files/add_date_to_files.py
+++ Pathlib_files/date_all_files/add_date_to_files.py
@@ -5,5 +5,15 @@ path = Path("files/December/a.txt")
 stats = path.stat()
 second_created = stats.st_ctime
 date_created = datetime.fromtimestamp(second_created)
-date_created_str =  date_created.strftime("%Y-%m-%d")
-print(date_created)
+date_created_str =  date_created.strftime("%Y-%m-%d_%H_%M_%S")
+
+root_file = Path("files")
+for path in root_file.glob("**/*"):
+    if path.is_file():
+        print(path)
+        print(path.name)
+        file_newname =date_created_str+"_"+path.name
+
+        new_filepath =  path.with_name(file_newname)
+        print(new_filepath)
+        path.rename(new_filepath)
