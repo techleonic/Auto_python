fba448664096d0ae181ef309e08f8844789370ba
techleonic <techleonic@gmail.com>
2024-08-14 09:28:01 -0600

    zip files

---
+++ Pathlib_files/create_emtyfiles_zipfiles/zip_files.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import zipfile
+
+root_dir = Path("files")
+archive_path = root_dir / Path("archive.zip")
+
+with zipfile.ZipFile(archive_path,"w") as zf:
+    for path in root_dir.rglob("*.txt"):
+        zf.write(path)
+        # deletes files
+        # path.unlink()
+        # deletes directories
+        # path.rmdir()
