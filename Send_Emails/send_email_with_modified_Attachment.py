a231a57930a8110baa28386bce2c379795ed6099
techleonic <techleonic@gmail.com>
2024-09-02 07:57:24 -0600

    send bills to emails

--- Send_Emails/send_email_with_modified_Attachment.py
+++ Send_Emails/send_email_with_modified_Attachment.py
@@ -12,13 +12,14 @@ yag = yagmail.SMTP(user=sender,password=sender_password)
 df = pandas.read_csv("emails.csv")
 
 def generate_file(name, content):
-    with open(f"{name}.txt","w") as file:
+    with open(name,"w") as file:
         file.write(content)
 
 for index, row in df.iterrows():
     content = f"name: {row["name"]}\nemail: {row["email"]}\nPayment: {row["amount"]}"
-    generate_file(row["name"],content)
-    email_content = f"hello {row["name"]} \nthis is your bill for our service:\n{row["amount"]}$"
+    file_name = row["name"] + ".txt"
+    generate_file(file_name,content)
+    email_content = [f"hello {row["name"]} \nthis is your bill for our service:\n{row["amount"]}$",file_name]
     yag.send(to=row["email"], subject="Service Payment",contents=email_content)
 
 
