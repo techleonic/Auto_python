1911e76bb217122495a35bb000efa08061ece9e9
techleonic <techleonic@gmail.com>
2024-09-14 06:44:25 -0600

    flask web app

---
+++ UI_flask_PyQt6/flask/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/", methods = ['POST'])
+def home_post():
+    dim1 = request.form["first_dim"]
+    dim2 = request.form["second_dim"]
+    dim3 = request.form["third_dim"]
+    volume = float(dim1) * float(dim2) * float(dim3)
+    return render_template("index.html", output = volume, dim_1=dim1, dim_2 = dim2,dim_3=dim3)
+
+
+if __name__ == "__main__":
+    app.run()
