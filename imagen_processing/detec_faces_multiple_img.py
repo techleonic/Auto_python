7cebee6860cc0b4db357ac5967b2959daedb4a4c
techleonic <techleonic@gmail.com>
2024-09-27 14:31:19 -0600

    detect faces in multiple img

--- imagen_processing/detec_faces_multiple_img.py
+++ imagen_processing/detec_faces_multiple_img.py
@@ -8,7 +8,7 @@ for img in main_dir.glob("*.jpeg"):
     faces = face_cascade.detectMultiScale(img_input, 1.1 , 4)
     if len(faces) > 0:
         for (x, y, w, h) in faces:
-            cv2.rectangle(img_input, (x, y), (x+w, y+h),(255, 255, 255), 4)
+            cv2.rectangle(img_input, (x, y), (x+w, y+h)git ,(255, 255, 255), 4)
     img_output = img_input
     cv2.imwrite(f"output/{img.name}", img_output)
 
