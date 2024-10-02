6182bb68e98d0d5b52d8dd148215ac36cf7f9e55
techleonic <techleonic@gmail.com>
2024-10-02 08:22:38 -0600

    get image from espcific frame

--- video_procesing/extract_frame_specific_time.py
+++ video_procesing/extract_frame_specific_time.py
@@ -1,1 +1,20 @@
 import cv2
+
+video = cv2.VideoCapture("video.mp4")
+nr_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+fps = video.get(cv2.CAP_PROP_FPS)
+print(nr_frames, fps)
+
+timestamp = '00:00:02.75'
+timestamp_list = timestamp.split(":")
+timestamp_list = [float(i) for i in timestamp_list]
+hours, minutes, seconds = timestamp_list
+
+framne_nr = (hours * 3600) * fps + (minutes * 60 * fps) + (seconds * fps)
+video.set(1, framne_nr)
+success, frame = video.read()
+print(success,frame)
+if success == True:
+    cv2.imwrite(f"frame{hours}-{minutes}-{seconds}.jpg", frame)
+else:
+    print("unable to find the frame")
