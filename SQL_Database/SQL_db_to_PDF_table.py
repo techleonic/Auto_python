0b1315ac315c4c0418ad93c587066c95af3a54d2
techleonic <techleonic@gmail.com>
2024-10-21 14:57:16 -0600

    inset data export as pdf from sql db

---
+++ SQL_Database/SQL_db_to_PDF_table.py
@@ -0,0 +1,28 @@
+import sqlite3
+from  fpdf import FPDF
+
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+
+cur.execute('PRAGMA table_info(ips)')
+columns = cur.fetchall()
+print(columns)
+
+pdf = FPDF(orientation='p', unit='pt', format='A4')
+pdf.add_page()
+
+for column in columns:
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100, h=25, txt=column[1], border=1)
+
+pdf.ln()
+cur.execute("SELECT * FROM 'ips'")
+rows= cur.fetchall()
+for row in rows:
+    for element in row:
+        pdf.set_font(family='Times', size=14)
+        pdf.cell(w=100, h=25, txt=str(element), border=1)
+    pdf.ln()
+pdf.output('output.pdf')
+
